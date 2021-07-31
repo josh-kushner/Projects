dba7fc52f0ecbb6dd1f0a200f3d3e3bd9bc66a1a
josh-kushner <87717746+josh-kushner@users.noreply.github.com>
2021-07-31 16:35:46 -0700

    Add files via upload

--- vehicle/vehicle.ino
+++ vehicle/vehicle.ino
@@ -1,14 +1,38 @@
-#include <L298NX2.h>
+#include <NewPing.h>
 
-#define EN_A 1
-#define IN1_A 2
-#define IN2_A 3
-#define EN_B 4
-#define IN1_B 5
-#define IN2_B 6
+//MOTOR 1 (MOTOR A, LEFT WHEEL)
+#define ENA 7
+#define IN1 6
+#define IN2 5
 
-//declare instance of both motors
-L298NX2 Motors(EN_A, IN1_A, IN2_A, EN_B, IN1_B, IN2_B);
+//MOTOR 2 (MOTOR B, RIGHT WHEEL)
+#define ENB 2
+#define IN3 4
+#define IN4 3 
+
+//MOTOR SPEEDS
+#define MIN_SPEED 130
+#define MAX_SPEED 255
+
+//FRONT SENSOR
+#define TRIGF 8
+#define ECHOF 9
+
+//LEFT SENSOR
+#define TRIGL 10 
+#define ECHOL 11
+
+//RIGHT SENSOR
+#define TRIGR 12
+#define ECHOR 13
+
+//DISTANCE THAT OBJECT IS DETECTED (inches)
+#define OBJ_DIST 18
+
+//sensors
+NewPing F_sensor(TRIGF, ECHOF, OBJ_DIST);
+NewPing L_sensor(TRIGL, ECHOL, OBJ_DIST);
+NewPing R_sensor(TRIGR, ECHOR, OBJ_DIST);
 
 
 //States
@@ -17,35 +41,171 @@ enum {
   DRIVE_FORWARD = 0,
   DRIVE_BACKWARD = 1,
   TURN_LEFT = 2,
-  TURN_RIGHT = 3,
-  TURN_AROUND = 4
+  TURN_RIGHT = 3
 };
 
-//State machine function
-void driveCheck() {
-  switch (State) {
-    case DRIVE_FORWARD:
-      break;
-    case DRIVE_BACKWARD:
-      break;
-    case TURN_LEFT:
-      break;
-    case TURN_RIGHT:
-      break;
-    case TURN_AROUND:
-      break;
-  }
+bool F_detected = false;
+bool L_detected = false;
+bool R_detected = false;
 
+void driveForward() {
+  //motor A (left wheel forward)
+  analogWrite(ENA, MIN_SPEED);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+
+  //motor B (right wheel forward)
+  analogWrite(ENB, MIN_SPEED);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+void driveBackward() {
+  //motor A (left wheel reverse)
+  analogWrite(ENA, MIN_SPEED);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+
+  //motor B (right wheel reverse)
+  analogWrite(ENB, MIN_SPEED);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+
+void stopCar() {
+  analogWrite(ENA, 0);
+  analogWrite(ENB, 0);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
+
+//turns vehicle left
+void turnLeft() {
+  //motor A (left wheel reverse)
+  analogWrite(ENA, MIN_SPEED);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+
+  //motor B (right wheel forward)
+  analogWrite(ENB, MIN_SPEED);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+//turns vehicle right
+void turnRight() {
+  //motor A (left wheel forward)
+  analogWrite(ENA, MIN_SPEED);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
   
+  //motor B (right wheel reverse)
+  analogWrite(ENB, MIN_SPEED);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+
+//checks if object detected in a specified sensor
+bool checkObject(String sensor) {
+  bool F_detected = false;
+  bool L_detected = false;
+  bool R_detected = false;
+  if (L_sensor.ping_in() <= OBJ_DIST) {
+    L_detected = true;
+    return true;
+   }
+   if (R_sensor.ping_in() <= OBJ_DIST) {
+    R_detected = true;
+    return true;
+   }
+   if (F_sensor.ping_in() <= OBJ_DIST) {
+    F_detected = true;
+    return true;
+  }
+  return false;
 }
 
 
+//State machine function
+void runCar() {
+  double L_sensor_dist;
+  double R_sensor_dist;
+  switch (State) {
+    case DRIVE_FORWARD:
+      driveForward();
+      if (checkObject()) {
+        //save L and R sensor distances, then state transition
+        L_sensor_dist = L_sensor.ping_in();
+        R_sensor_dist = R_sensor.ping_in();
+        State = DRIVE_BACKWARD;
+      }
+      break;
+    case DRIVE_BACKWARD:
+      driveBackward();
+      
+      //stop driving backwards after this distance
+      if (L_sensor.ping_in() >= L_sensor_dist + (OBJ_DIST/2)) {
+
+        //see which direction has more space to turn the vehicle
+        if (L_detected || (F_detected && L_detected)) {
+          State = TURN_RIGHT;
+        }
+        else if (R_detected || (F_detected && R_detected)) {
+          State = TURN_RIGHT;
+        }
+        else if (F_detected || (L_detected && R_detected) || (F_detected && L_detected && R_detected)) {
+          if (R_sensor_dist <= L_sensor_dist) {
+            State = TURN_RIGHT;
+          }
+          else if (L_sensor_dist < R_sensor_dist) {
+            State = TURN_LEFT;
+          }
+        }
+      }
+      break;
+    case TURN_LEFT:
+      turnLeft();
+      if (!checkObject()) {
+        State = DRIVE_FORWARD;
+      }
+      break;
+    case TURN_RIGHT:
+      turnRight();
+      if (!checkObject)) {
+        State = DRIVE_FORWARD;
+      }
+      break;
+  }
+}
 
 void setup() {
   Serial.begin(9600);
 
+  //motor pins
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+  //sensor pins
+  pinMode(ECHOF, INPUT);
+  pinMode(ECHOR, INPUT);
+  pinMode(ECHOL, INPUT);
+  pinMode(TRIGF, OUTPUT);
+  pinMode(TRIGR, OUTPUT);
+  pinMode(TRIGL, OUTPUT);
+
+  //initlialize motors
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
 }
 
 void loop() {
-
+  //runCar();
 }
